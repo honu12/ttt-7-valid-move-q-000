41eca1dc52ac2debfbc396a9be3dc1946b2ab9f7
Honu <honu.menehune@gmail.com>
2015-12-01 14:56:50 +0000

    nitrous-backup at Tue Dec  1 14:56:50 UTC 2015

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,7 @@
 # code your #valid_move? method here
-
+def valid_move?(board, position)
+  if position > 9
+    puts "Please choose a number 1 -9"
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
